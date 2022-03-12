6cf2a2aec0f0cf9aaef6a54e500ea73b3753ae0b
Code-Deer <42050666+Code-Deer@users.noreply.github.com>
2022-03-12 17:59:05 +0800

    Update Mediapipe_Pose.py

--- Mediapipe_Pose.py
+++ Mediapipe_Pose.py
@@ -9,41 +9,6 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 prevTime = 0
-keyPoint = [
-    "nose",
-    "left_eye_inner",
-    "left_eye",
-    "left_eye_outer",
-    "right_eye_inner",
-    "right_eye",
-    "right_eye_outer",
-    "left_ear",
-    "right_ear",
-    "mouth_left",
-    "mouth_right",
-    "left_shoulder",
-    "right_shoulder",
-    "left_elbow",
-    "right_elbow",
-    "left_wrist",
-    "right_wrist",
-    "left_pinky",
-    "right_pinky",
-    "left_index",
-    "right_index",
-    "left_thumb",
-    "right_thumb",
-    "left_hip",
-    "right_hip",
-    "left_knee",
-    "right_knee",
-    "left_ankle",
-    "right_ankle",
-    "left_heel",
-    "right_heel",
-    "left_foot_index",
-    "right_foot_index"
-]
 keyXYZ = [
     "nose_x",
     "nose_y",
